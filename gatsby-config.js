fc6264eadd0daffffbc92b0d9a83f62cad817163
Eason Chiang <ishiu.chiang@gmail.com>
2019-09-14 23:10:48 +0800

    add create client paths plugin for client only path

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,10 @@ module.exports = {
         icon: `src/images/paper-note-icon.png`,
       },
     },
+    {
+      resolve: `gatsby-plugin-create-client-paths`,
+      options: { prefixes: [`/notes/*`] },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
